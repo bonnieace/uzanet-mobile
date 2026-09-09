3186371572618942d1c3659f3c2372b9de114c0f
wamweki boniface masota <94840519+bonnieace@users.noreply.github.com>
2026-09-09 23:55:09 +0300

    Make Uzanet API the mobile entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:uzanet/homepage.dart';
 
+import 'api/uzanet_api.dart';
+import 'screens/auth_gate.dart';
 import 'theme/theme.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final UzanetApi _api;
+
+  @override
+  void initState() {
+    super.initState();
+    _api = UzanetApi();
+  }
+
+  @override
+  void dispose() {
+    _api.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'UzaNet',
+      title: 'Uzanet',
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
-      home:StatusPage(),
+      home: AuthGate(api: _api),
     );
   }
 }
-
-
